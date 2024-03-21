4d8fa43e94a5ba20768011938794e7219f091e8e
George Khalil <159185251+George-Khalil-29@users.noreply.github.com>
2024-03-21 18:42:37 +0000

    update the input

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 George, Khalil All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: George, Khalil
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates area of a triangle.
+ */
+function calculatePermeterAndAreaOfRectangle () {
+  // input
+  const baseoftriangle = parseInt(document.getElementById('Base-Of-Triangle').value)
+  const heightoftriangle = parseInt(document.getElementById('Height-Of-Triangle').value)
+
+  // process
+  const areaOfTriangle = baseoftriangle * heightoftriangle /2
+
+  // output
+  document.getElementById('area').innerHTML = 
+  "Area is: " + areaOfTriangle+ ' cm²'
+}
